7ac168339e9f0463f82db1c7630e02d0c8768607
tpg-dhe <127889279+tpg-dhe@users.noreply.github.com>
2023-03-23 20:11:44 +0000

    Exercise 20230323

--- 01_restaurant/challenges/01_07_teilnahmeevent_ch.sql
+++ 01_restaurant/challenges/01_07_teilnahmeevent_ch.sql
@@ -1,3 +1,13 @@
 -- Registrieren Sie einen Kunden 'atapley2j@kinetecoinc.com' 
 -- für unsere Jubilaeumsfeier.
 -- Der Kunde wird mit drei Freunden an der Feier teilnehmen.
+
+INSERT INTO Jubelaeum
+  (KundenID, AnzahlTeilnehmer)
+VALUES(
+    (SELECT KundenID
+    FROM Kunden
+    WHERE Email = "atapley2j@kinetecoinc.com")
+    ,4)
+
+SELECT * FROM Jubelaeum
--- 01_restaurant/challenges/01_08_findereservierung_ch.sql
+++ 01_restaurant/challenges/01_08_findereservierung_ch.sql
@@ -4,3 +4,9 @@
 -- Der Name hat unter anderem folgenede Variationen:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 -- Heute ist der 14.Juni.
+
+SELECT Kunden.KundenID, Kunden.Nachname, Reservierung.Datum, Reservierung.PartyGroesse
+FROM Reservierung
+JOIN Kunden ON Reservierung.KundenID = Kunden.KundenID
+WHERE Nachname LIKE 'ste%' AND PartyGroesse = 4
+ORDER BY Datum DESC
